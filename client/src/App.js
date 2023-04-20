0ea8b6d0f1ef2dbe8570fbe22a6d0a4cbb296b7d
Elton Mehmeti <97539294+EltonMehmeti@users.noreply.github.com>
2023-04-20 15:01:13 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
   let [count, setCount] = useState(0);
@@ -11,22 +11,24 @@ function App() {
       <p className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
         {count}
       </p>
-      <button
-        className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        Click !
-      </button>
-      <button
-        className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-        onClick={() => {
-          setCount((count = 0));
-        }}
-      >
-        Reset
-      </button>
+      <div className="flex flex-row gap-10 pt-4">
+        <button
+          className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          Click !
+        </button>
+        <button
+          className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+          onClick={() => {
+            setCount((count = 0));
+          }}
+        >
+          Reset
+        </button>
+      </div>
     </div>
   );
 }

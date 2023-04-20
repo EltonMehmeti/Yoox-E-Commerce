8c94a4d722489ae8d9db45054b2ea0c492d13e6a
Elton Mehmeti <97539294+EltonMehmeti@users.noreply.github.com>
2023-04-20 15:23:17 +0200

    sen

---
+++ client/src/components/Header.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Header = () => {
+  let [count, setCount] = useState(0);
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-[100vh]">
+      <img src={logo} width={"400px"} />
+      <br />
+      <h1 className="mb-3 text-lg text-gray-500 md:text-xl dark:text-gray-400">
+        Hello Ekipa ma e fort qetu i thirrni komponentat qe duhet!
+      </h1>
+      <p className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        {count}
+      </p>
+      <div className="flex flex-row gap-10 pt-4">
+        <button
+          className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          Click !
+        </button>
+        <button
+          className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+          onClick={() => {
+            setCount((count = 0));
+          }}
+        >
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ client/src/pages/SignIn.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SignIn = () => {
+  return (
+    <div>
+      <h1>SignIn</h1>
+      <p>shkruni html qetu</p>
+    </div>
+  );
+};
+
+export default SignIn;

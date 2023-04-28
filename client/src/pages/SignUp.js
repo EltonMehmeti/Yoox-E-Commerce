9d0fb6d28fe99afe346715d917ac814781ffdfda
Elton Mehmeti <97539294+EltonMehmeti@users.noreply.github.com>
2023-04-28 23:32:11 +0200

    updateSigup confilct

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import logo2 from "../img/logo2.png";
 import logoFigure from "../img/loginFigure.png";
@@ -31,12 +30,6 @@ const SignUp = () => {
       });
   };
 
-
-import React from "react";
-import logoFigure from "../img/loginFigure.png";
-import logo2 from "../img/logo2.png";
-const SignUp = () => {
-
   return (
     <div className="h-screen w-full flex bg-orange-50">
       <div className="grid grid-cols-1 md:grid-cols-2 m-auto  shadow-lg bg-orange-50 sm:max-w-[1000px] h-[570px] rounded-lg">
@@ -48,11 +41,9 @@ const SignUp = () => {
           <div className="flex flex-col py-2 mb-2">
             <label className="text-xs ">Name</label>
             <input
-
               onChange={(e) => {
                 setName(e.target.value);
               }}
-
               name="name"
               className="text-xs border border-transparent rounded-md shadow-lg shadow-gray-300  bg-orange-100 border p-2 border relative bg-red-50  rounded shadow-gray-300"
               type="text"
@@ -62,11 +53,9 @@ const SignUp = () => {
           <div className="flex flex-col py-2 mb-2">
             <label className="text-xs ">Email</label>
             <input
-
               onChange={(e) => {
                 setEmail(e.target.value);
               }}
-
               name="email"
               className="text-xs border border-transparent rounded-md shadow-lg shadow-gray-300  bg-orange-100 border p-2 border relative bg-red-50  rounded shadow-gray-300"
               type="text"
@@ -76,16 +65,11 @@ const SignUp = () => {
           <div className="flex flex-col py-2 mb-2">
             <label className="text-xs ">Password</label>
             <input
-
               onChange={(e) => {
                 setPassword(e.target.value);
               }}
               name="password"
               className="text-xs border py-2 border relative bg-orange-100 rounded-md  border border-transparent shadow-lg shadow-gray-300 "
-
-              name="password"
-              className="text-xs border border-transparent rounded-md shadow-lg shadow-gray-300  bg-orange-100 border p-2 border relative bg-red-50  rounded shadow-gray-300 "
-
               type="password"
               placeholder="********"
             />
@@ -93,11 +77,9 @@ const SignUp = () => {
           <div className="flex flex-col py-2 mb-2">
             <label className="text-xs ">Address</label>
             <input
-
               onChange={(e) => {
                 setAddress(e.target.value);
               }}
-
               name="address"
               className="text-xs border border-transparent rounded-md shadow-lg shadow-gray-300  bg-orange-100 border p-2 border relative bg-red-50  rounded shadow-gray-300"
               type="text"
@@ -108,11 +90,9 @@ const SignUp = () => {
             <div className="flex flex-col py-2 pr-4 mb-0">
               <label className="text-xs ">City</label>
               <input
-
                 onChange={(e) => {
                   setCity(e.target.value);
                 }}
-
                 name="city"
                 className="w-full text-xs border border-transparent rounded-md shadow-lg shadow-gray-300  bg-orange-100 border p-2 border relative bg-red-50  rounded shadow-gray-300"
                 type="text"
@@ -122,11 +102,9 @@ const SignUp = () => {
             <div className="flex flex-col py-2 pl-4 m-0">
               <label className="text-xs ">Phone</label>
               <input
-
                 onChange={(e) => {
                   setPhone(e.target.value);
                 }}
-
                 name="phone"
                 className="w-full text-xs border border-transparent rounded-md shadow-lg shadow-gray-300  bg-orange-100 border p-2 border relative bg-red-50  rounded shadow-gray-300"
                 type="text"
@@ -134,14 +112,10 @@ const SignUp = () => {
               />
             </div>
           </div>
-
           <button
             onClick={submitUser}
             className="text-xs border w-full bg-orange-500 hover:bg-orange-600 border-2 text-white  border border-transparent rounded-lg my-3 py-2"
           >
-
-          <button className="text-xs border w-full bg-orange-500 hover:bg-orange-600 border-2 text-white  border border-transparent rounded-lg my-3 py-2">
-
             Sign Up
           </button>
         </form>

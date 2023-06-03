abd9f4d57d7b0c1b7037e1e7345b1809cde5c861
Elton Mehmeti <97539294+EltonMehmeti@users.noreply.github.com>
2023-06-03 23:39:48 +0200

    New Cart Design

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -32,6 +32,9 @@ const SignIn = () => {
         } else if (response.data[0].User_Type == "CS") {
           navigate("/costumerS");
           setLoginStatus(response.data[0].Name);
+        } else if (response.data[0].User_Type == "Post") {
+          navigate("/shipping");
+          setLoginStatus(response.data[0].Name);
         }
       });
   };
